6c0e0ce6de3e496414d8a7d32d7ca1749c995fe6
unknown <dian_t_d@yahoo.co.uk>
2023-06-04 19:38:56 +0100

    add avp description

--- docs/js/index.js
+++ docs/js/index.js
@@ -4,60 +4,59 @@ const response = await fetch(`${pathname}game.json`);
 const { url, controls, aspectRatio } = await response.json();
 window.RufflePlayer = window.RufflePlayer || {};
 const $playground = document.querySelector('.playground');
+const $playButton = document.querySelector('.play-button')
 const $controls = document.querySelector('.controls');
-const run = () => {
+console.log(222, document.querySelector('.play-button'));
+document.querySelector('.play-button').onclick = () => {
     document.body.classList.add('run');
-    $playground.onclick = null;
-};
-const attachPlayer = () => {
     $playground.prepend(player);
     player.load(`${pathname}/${url}`);
-    $playground.onclick = run;
 };
 $playground.style.aspectRatio = aspectRatio;
 document.querySelector('.button-close').onclick = (event) => {
     event.preventDefault();
     document.body.classList.remove('run');
-    console.log(111, player.ensureFreshInstance);
-    player.ensureFreshInstance();
-    //player.remove();
-    //$playground.prepend(player);
-    
-    //$playground.onclick = run;
-    setTimeout(attachPlayer, 1000)
+    player.remove();
 }
 const ruffle = window.RufflePlayer.newest();
 const player = ruffle.createPlayer();
 player.config = {
+    autoplay: 'on',
     contextMenu: 'rightClickOnly'
 };
 const triggerKeydownEvent = event => window.dispatchEvent(new KeyboardEvent('keydown', event));
 const triggerKeyupEvent = event => window.dispatchEvent(new KeyboardEvent('keyup', event));
-attachPlayer();
-controls.forEach(async (control) => {
-    const { type } = control;
-    if ('joystick' === type) {
-        const { mappings, dataset } = control;
-        const mapKeydown = direction => triggerKeydownEvent({ code: mappings[direction] });
-        const mapKeyup = direction => triggerKeyupEvent({ code: mappings[direction] });
-        $controls.insertAdjacentHTML('beforeend', '<virtual-joystick></virtual-joystick>');
-        const $joystick = document.querySelector('virtual-joystick');
-        $joystick.addEventListener('joystickdown', () => {
-            $joystick.dataset.capture.split('').forEach(mapKeydown);
-        });
-        $joystick.addEventListener('joystickmove', () => {
-            $joystick.dataset.release.split('').forEach(mapKeyup);
-            $joystick.dataset.capture.split('').forEach(mapKeydown);
-        });
-        $joystick.addEventListener('joystickup', () => {
-            $joystick.dataset.release.split('').forEach(mapKeyup);
-        });
-        if (dataset) {
-            Object.assign($joystick.dataset, dataset);
+if (controls.length) {
+    $controls.insertAdjacentHTML('beforeend', '<button type="button" class="button button-toggle-controls"></button>');
+    $controls.lastElementChild.onclick = ({ currentTarget }) => {
+        currentTarget.classList.toggle('hide-gamepad');
+    }
+    controls.forEach(async (control) => {
+        const { type } = control;
+        if ('joystick' === type) {
+            const { mappings, dataset } = control;
+            const mapKeydown = direction => triggerKeydownEvent({ code: mappings[direction] });
+            const mapKeyup = direction => triggerKeyupEvent({ code: mappings[direction] });
+            $controls.insertAdjacentHTML('beforeend', '<virtual-joystick></virtual-joystick>');
+            const $joystick = $controls.querySelector('virtual-joystick');
+            $joystick.addEventListener('joystickdown', () => {
+                $joystick.dataset.capture.split('').forEach(mapKeydown);
+            });
+            $joystick.addEventListener('joystickmove', () => {
+                $joystick.dataset.release.split('').forEach(mapKeyup);
+                $joystick.dataset.capture.split('').forEach(mapKeydown);
+            });
+            $joystick.addEventListener('joystickup', () => {
+                $joystick.dataset.release.split('').forEach(mapKeyup);
+            });
+            if (dataset) {
+                Object.assign($joystick.dataset, dataset);
+            }
         }
-    }
-    if ('button' === type) {
-        const Button = await import('./button.js');
-        Button.default(control, $controls);
-    }
-});
+        if ('button' === type) {
+            const Button = await import('./button.js');
+            Button.default(control, $controls);
+        }
+    });
+}
+
