2189f912265a0effff4b608ba56a975d6f69deda
unknown <dian_t_d@yahoo.co.uk>
2023-06-25 15:23:55 +0100

    Aspect ratio to width and height

--- scrape/html.js
+++ scrape/html.js
@@ -12,21 +12,15 @@ fs.readdir('../docs/s/', (err, folders) => {
         const html = fs.readFileSync(ref, 'utf8');
         const dom = new JSDOM(html);
         const extractAspectRatio = () => {
-            const { style } = dom.window.document.querySelector('.playground');
-            if (style) {
-                console.log(111, folder, style._values['aspect-ratio']);
-                return "style.replace('aspect-ratio: ', '').replace(';', '')"
-            }
-            return '';
+            const { _values } = dom.window.document.querySelector('.playground').style;
+            return { w: _values['--w'], h: _values['--h']};
         };
-        extractAspectRatio();
-        // console.log(111, folder.toString(), dom.window.document.querySelector('.playground').style)
-        /* const addedOn = dom.window.document.querySelector('.added-on')?.textContent || '';
-        const locals = { title: name, description, addedOn, aspectRatio: extractAspectRatio() };
+        const addedOn = dom.window.document.querySelector('.added-on')?.textContent || '';
+        const locals = { title: name, description, addedOn, ...extractAspectRatio() };
         es6Renderer(
             './templates/index.html',
             { locals },
             (err, content) => fs.writeFileSync(ref, content)
-        ); */
+        );
     });
 });
