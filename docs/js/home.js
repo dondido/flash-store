895a6ab2b56b5049111e7e2992fe03ade8fd0085
unknown <dian_t_d@yahoo.co.uk>
2023-07-11 19:11:12 +0100

    Add video lazy loading

--- docs/js/home.js
+++ docs/js/home.js
@@ -1,23 +1,37 @@
-let activeVideos = [];
+let activeVideo = null;
 const up = ({ target }) => target.load();
-const move = ({ target, pointerId }) => {
-    /* if (target.classList.contains('game-link-video')) {
-        target.setPointerCapture(pointerId);
-        target.play();
-        activeVideos.push(target);
+const move = ({ target }) => {
+    if (target.tagName === 'VIDEO') {
+        if (activeVideo === null) {
+            target.load();
+            target.play();
+            activeVideo = target;
+        }
     }
     else {
-        activeVideos.forEach(activeVideo => activeVideo.load());
-        activeVideos = [];
-    } */
-};
-const click = (event) => {
-    const link = event.target.closest('.game-link');
-    if (link) {
-        event.preventDefault();
-        location.href = link.href;
-        alert(link.href);
+        activeVideo?.load();
+        activeVideo = null;
     }
 };
 document.addEventListener('pointermove', move);
-document.addEventListener('pointerdown', click);
+document.addEventListener('DOMContentLoaded', function () {
+    const lazyVideos = [...document.querySelectorAll('video.lazy')];
+    if ('IntersectionObserver' in window) {
+        const lazyVideoObserver = new IntersectionObserver((entries) => {
+            entries.forEach((video) => {
+                if (video.isIntersecting) {
+                    video.target.poster = video.target.dataset.poster;
+                    for (const source in video.target.children) {
+                        const videoSource = video.target.children[source];
+                        if (typeof videoSource.tagName === 'string' && videoSource.tagName === 'SOURCE') {
+                            videoSource.src = videoSource.dataset.src;
+                        }
+                    }
+                    video.target.classList.remove('lazy');
+                    lazyVideoObserver.unobserve(video.target);
+                }
+            });
+        });
+        lazyVideos.forEach(lazyVideo => lazyVideoObserver.observe(lazyVideo));
+    }
+});
