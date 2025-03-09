0d25b23f1022c39462f9ebb394545bb54e58070d
Dian Dimitrov <dian_t_d@yahoo.co.uk>
2025-03-09 13:11:50 +0000

    Update gradle build

--- android/FlashGameEmulator/app/build.gradle.kts
+++ android/FlashGameEmulator/app/build.gradle.kts
@@ -60,7 +60,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    testImplementation(libs.junit)
+    implementation(libs.androidx.activity.ktx)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
